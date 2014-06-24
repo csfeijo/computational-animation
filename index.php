c1e52ca634dd070cbfa0d60da1410f9bd66f8a23
Cícero Feijó <cicerol.feijo@hp.com>
2014-06-24 18:57:10 -0300

    Added files of basic prototype version.

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<title>Cícero Feijó - Spring Animation</title>
+		<link rel="stylesheet" type="text/css" href="_css/context.css"/>
+	</head>
+<body>
+
+	<canvas id="c"></canvas>
+
+	
+	<div id="credit">
+		<p><strong>Cícero Feijó</strong></p>
+		<p><strong>About:</strong>Spring Animation</p>
+	</div>
+
+	<script src="_js/context.js"></script>
+	
+</body>
+</html>
